ba958e9e30422842add956a47295a88ff9cff2bb
Christopher Magnuson <magnus32@uwm.edu>
2018-02-15 09:06:56 -0600

    Update gitignores and a little file formatting

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
- // res.render('index', { title: 'Professional Development AF' })
+router.get('/', function (req, res, next) {
+    // res.render('index', { title: 'Professional Development AF' })
     var db = req.db;
     var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
+    collection.find({}, {}, function (e, docs) {
         res.render('index', {
-            "userlist" : docs , title : 'Professional Development AF'
+            "userlist": docs, title: 'Professional Development AF'
         });
     });
 });
 
 / GET Hello World page. /
-router.get('/helloworld', function(req, res) {
-    res.render('helloworld', { title: 'Hello, World!' });
+router.get('/helloworld', function (req, res) {
+    res.render('helloworld', {title: 'Hello, World!'});
 });
 module.exports = router;
