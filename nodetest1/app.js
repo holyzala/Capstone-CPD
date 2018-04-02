dedbfeb1c051e10b8559f4783c74935a4f643fc1
yourguyphil <philips.nguyen96@gmail.com>
2018-04-01 20:36:15 -0500

    Code Clean up (Removing Tutorial Stuff)

--- nodetest1/app.js
+++ nodetest1/app.js
@@ -18,7 +18,6 @@ ret.catch(function(error) {
 });
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -42,7 +41,6 @@ app.use(function (req, res, next) {
 });
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
