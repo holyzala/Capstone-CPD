588cb5282c48ab7349cd84aaf2f0fc5cb16a24ca
Christopher Magnuson <magnus32@uwm.edu>
2018-04-17 11:46:26 -0500

    First step of rest

--- nodetest1/app.js
+++ nodetest1/app.js
@@ -18,12 +18,9 @@ ret.catch(function(error) {
 });
 
 var index = require('./routes/index');
+var api = require('./routes/api');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -40,7 +37,12 @@ app.use(function (req, res, next) {
     next();
 });
 
+app.use('/api', api);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use('/', index);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
